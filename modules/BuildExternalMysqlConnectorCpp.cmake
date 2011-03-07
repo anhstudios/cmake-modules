782fd4dcf6d5e431e89af648d165407ed4ee0562
Eric Barr <eb@anhstudios.com>
2011-03-07 10:09:12 -0800

    Updated the version of mysql connector c++ being built.

--- modules/BuildExternalMysqlConnectorCpp.cmake
+++ modules/BuildExternalMysqlConnectorCpp.cmake
@@ -5,7 +5,7 @@ ExternalProject_Add(MysqlConnectorCpp
 	DEPENDS Boost MysqlConnectorC
 	PREFIX ${VENDOR_PREFIX}	
 	GIT_REPOSITORY https://github.com/anhstudios/mysql-connector-cpp.git
-	GIT_TAG 94c4bf8
+	GIT_TAG 22bf29c
 	UPDATE_COMMAND ""
 	PATCH_COMMAND ""
 	BUILD_IN_SOURCE 1
