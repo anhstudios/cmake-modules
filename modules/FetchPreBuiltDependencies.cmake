d7c8b0e49a467e715e99fb608b44174c2aa87eff
Eric Barr <eb@anhstudios.com>
2011-06-01 02:11:24 -0400

    Updated to latest dependencies

--- modules/FetchPreBuiltDependencies.cmake
+++ modules/FetchPreBuiltDependencies.cmake
@@ -4,7 +4,7 @@ set(vendor_libraries vendor)
 ExternalProject_Add(vendor
 	DOWNLOAD_DIR ${CMAKE_CURRENT_BINARY_DIR}
 	URL http://s3.amazonaws.com/github/downloads/anhstudios/swganh/swganh-deps-${swganh_VERSION_MAJOR}.${swganh_VERSION_MINOR}.${swganh_VERSION_PATCH}-win.tar.gz
-	URL_MD5 9ee76ac20f8165468f5c363618d144b2
+	URL_MD5 09dfb24228fe51be1bbe32400e7ffe7e
 	SOURCE_DIR ${VENDOR_PREFIX}
 	UPDATE_COMMAND ""
 	PATCH_COMMAND ""
